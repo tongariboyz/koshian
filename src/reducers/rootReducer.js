08ef5c06664e8df934ca5b681b49fc395b765f68
ideyuta <ide.yuta@gmail.com>
2015-10-20 17:24:45 +0900

    feat(reducers): rootReducer に period を追加

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux';
 import {login} from './login';
+import {period} from './period';
 
 
-const rootReducer = combineReducers({login});
+const rootReducer = combineReducers({login, period});
 
 export default rootReducer;
