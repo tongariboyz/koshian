a211ccf1c6f76ea5d0fbaa516aac745e39227bfc
ideyuta <ide.yuta@gmail.com>
2015-10-21 11:28:34 +0900

    feat(actions): 表示する期間を変更するアクションを追加

--- src/actions/period.js
+++ src/actions/period.js
@@ -1,4 +1,7 @@
-import {CHANGE_PERIOD_VIEW_INDEX} from '../constants/period';
+import {
+  CHANGE_PERIOD_VIEW_INDEX,
+  CHANGE_VIEW_PERIOD
+} from '../constants/period';
 
 /**
  * PeriodScrollView の表示中の index を変更
@@ -10,5 +13,18 @@ export function changePeriodViewIndex(index) {
   return {
     type: CHANGE_PERIOD_VIEW_INDEX,
     payload: {index}
+  };
+}
+
+/**
+ * 表示する期間を変更
+ *
+ * @param {number} direction direction
+ * @return {function}
+ */
+export function changeViewPeriod(direction) {
+  return {
+    type: CHANGE_VIEW_PERIOD,
+    payload: {direction}
   };
 }
