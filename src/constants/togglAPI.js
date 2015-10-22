245f5e5b54b06999eabd4abd9503ae48f89137d6
moqada <moqada@gmail.com>
2015-10-22 23:24:40 +0900

    feat(helpers): TogglAPIClientを追加

---
+++ src/constants/togglAPI.js
@@ -0,0 +1,3 @@
+export const API_ENDPOINT = 'https://www.toggl.com/api/v8';
+export const API_METHODS = ['get', 'post'];
+export const TIME_FORMAT = 'YYYY-MM-DDTHH:mm:ssZ';
---
+++ test/helpers/TogglAPIClient-spec.js
@@ -0,0 +1,139 @@
+/* eslint camelcase: 0 */
+import assert from 'power-assert';
+import nock from 'nock';
+import {API_ENDPOINT} from '../../src/constants/togglAPI';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+
+
+describe('helpers/TogglAPIClient', () => {
+  const dummyAPIToken = 'dummy_token';
+  let APIClient = null;
+  let apiClient = null;
+  let sandbox = null;
+  let mockServer = null;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    APIClient = proxyquire('../../src/helpers/TogglAPIClient', {});
+    apiClient = new APIClient(dummyAPIToken);
+    mockServer = nock(API_ENDPOINT);
+  });
+  afterEach(() => {
+    sandbox.restore();
+    nock.cleanAll();
+  });
+
+  describe('api methods', () => {
+    const stubs = {
+      ins: {},
+      cls: {}
+    };
+    const stubRets = {
+      ins: {
+        get: 'called ins get method',
+        post: 'called ins post method'
+      },
+      cls: {
+        get: 'called cls get method'
+      }
+    };
+    beforeEach(() => {
+      stubs.ins.get = sandbox.stub(apiClient, 'get', () => stubRets.ins.get);
+      stubs.ins.post = sandbox.stub(apiClient, 'post', () => stubRets.ins.post);
+      stubs.cls.get = sandbox.stub(APIClient, 'get', () => stubRets.cls.get);
+    });
+
+    it('.login()', () => {
+      const ret = APIClient.login('user', 'pass');
+      assert(ret === stubRets.cls.get);
+      assert(stubs.cls.get.args.length === 1);
+      assert.deepEqual(stubs.cls.get.args[0], [
+        '/me', {
+          headers: {
+            Authorization: 'Basic dXNlcjpwYXNz'
+          }
+        }
+      ]);
+    });
+
+    it('#getTimeEntries()', () => {
+      const start = '2015-09-08T00:00:00+09:00';
+      const end = '2015-09-09T00:00:00+09:00';
+      const ret = apiClient.getTimeEntries(new Date(start), new Date(end));
+      assert(ret === stubRets.ins.get);
+      assert(stubs.ins.get.args.length === 1);
+      assert.deepEqual(stubs.ins.get.args[0], [
+        '/time_entries', {
+          query: {
+            start_date: start,
+            end_date: end
+          }
+        }
+      ]);
+    });
+  });
+
+  describe('base methods', () => {
+    describe('#get()', () => {
+      it('headers に api_token を含む', () => {
+        mockServer.get('/dummy').reply(200, {dummy: 'dummy'});
+        return apiClient.get('/dummy').then(ret => {
+          assert(ret.request.headers[dummyAPIToken] === 'api_token');
+        });
+      });
+    });
+    describe('.get', () => {
+      it('headers に api_token を含まない', () => {
+        mockServer.get('/dummy').reply(200, {dummy: 'dummy'});
+        return APIClient.get('/dummy').then(ret => {
+          assert(ret.request.headers[dummyAPIToken] === undefined);
+        });
+      });
+
+      it('opts.headers が指定された場合 headers をセットする', () => {
+        mockServer.get('/dummy').reply(200, {dummy: 'dummy'});
+        const headers = {
+          hoge: 'fuga',
+          foo: 'bar'
+        };
+        return APIClient.get('/dummy', {headers}).then(ret => {
+          assert(ret.request.headers.hoge === headers.hoge);
+          assert(ret.request.headers.fuga === headers.fuga);
+        });
+      });
+
+      it('opts.query が指定された場合 query をセットする', () => {
+        mockServer.get('/dummy').query(true).reply(200, {dummy: 'dummy'});
+        const query = {
+          hoge: 'fuga',
+          foo: 'bar'
+        };
+        return APIClient.get('/dummy', {query}).then(ret => {
+          assert.deepEqual(ret.request.query, query);
+        });
+      });
+    });
+
+    describe('.post', () => {
+      it('opst.data が指定された場合 json を送信する', () => {
+        mockServer.post('/dummy').reply(200, {dummy: 'dummy'});
+        const data = {
+          hoge: 'fuga',
+          foo: 'bar'
+        };
+        return APIClient.post('/dummy', {data}).then(ret => {
+          assert.deepEqual(ret, {
+            body: {dummy: 'dummy'},
+            headers: {'content-type': 'application/json'},
+            request: {
+              data,
+              headers: {},
+              query: {}
+            }
+          });
+        });
+      });
+    });
+  });
+});
