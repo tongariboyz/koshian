28bd4b7d4cc4343ac4c95f743e6289f378a48aab
moqada <moqada@gmail.com>
2015-10-23 12:41:28 +0900

    fix(middlewares): clientMiddlewareのdispatchまわり修正

--- src/middlewares/client.js
+++ src/middlewares/client.js
@@ -4,17 +4,17 @@
  * @param {function} getState getState
  * @return {function}
  */
-export default function clientMiddleware({getState}) {
+export default function clientMiddleware({dispatch, getState}) {
   return next => action => {
     const state = getState();
     if (action.meta && action.meta.client) {
       const {client, ...others} = action.meta;
-      next(Object.assign({}, action, {meta: others}));
-      return next(Object.assign({
+      dispatch(Object.assign({
         type: client.type,
         payload: client.next(state.auth.client),
         meta: others
       }));
+      return next(Object.assign({}, action, {meta: others}));
     }
     return next(action);
   };
