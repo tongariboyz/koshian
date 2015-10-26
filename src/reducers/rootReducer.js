26b2ac490cb5a3728e4b11eba370d23b568b295c
ideyuta <ide.yuta@gmail.com>
2015-10-26 10:45:59 +0900

    feat(reducers): RootReducer に timer を追加

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,9 @@
 import {combineReducers} from 'redux';
 import {auth} from './auth';
 import {period} from './period';
+import {timer} from './timer';
 
 
-const rootReducer = combineReducers({auth, period});
+const rootReducer = combineReducers({auth, period, timer});
 
 export default rootReducer;
