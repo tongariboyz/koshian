02e38210ff66b91d914a2020a221a3ad4a0f4c2c
ideyuta <ide.yuta@gmail.com>
2015-10-29 11:55:52 +0900

    fix(constants): periodActionTypes を修正

--- src/constants/periodActionTypes.js
+++ src/constants/periodActionTypes.js
@@ -5,7 +5,7 @@ import reaction from 'dacho';
 // TODO: Add flowtype for dacho module
 const types: {[type: string]: string} = reaction([
   'CHANGE_PERIOD_VIEW_INDEX',
-  'CHANGE_VIEW_PERIOD',
+  'FETCH_TIME_ENTRIES',
   'INITIALIZE_PERIOD_VIEW_INDEX',
   'RESPONSE_PERIOD_TIME_ENTRIES'
 ], 'PERIOD/');
