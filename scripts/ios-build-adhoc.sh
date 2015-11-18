dde98c0d222c1aeed43b15113382a9eff483009c
moqada <moqada@gmail.com>
2015-11-18 19:52:10 +0900

    chore(scripts): iOS用ビルド処理をshファイル化

---
+++ scripts/ios-build-adhoc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+APPLE_AUTHORITY_BASE64= \
+  DISTRIBUTION_CERTIFICATE_BASE64= \
+  DISTRIBUTION_KEY_BASE64= \
+  DEPLOYGATE_API_TOKEN=  \
+  DEPLOYGATE_USERNAME= \
+  ITUNES_CONNECT_ACCOUNT= \
+  ITUNES_CONNECT_PASSWORD= \
+  KEY_PASSWORD= \
+  bundle exec ipa build \
+    --verbose \
+    --project "$XCODE_PROJECT" \
+    --scheme "$APPNAME" \
+    --configuration Release \
+    --destination Distribution/AdHoc
